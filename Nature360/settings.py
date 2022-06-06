6113f7c5970a78f3c9406c5493455a5cb1c24f21
prashant-paliwal <92542691+prashant-paliwal@users.noreply.github.com>
2022-06-06 17:09:48 +0530

    Update settings.py

--- Nature360/settings.py
+++ Nature360/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-87#=cxhvtve*y5@)_mx^r5fs2o8s&-_s#ux)mf^%g0gxdc09as
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
