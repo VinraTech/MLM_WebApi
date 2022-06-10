6bdae2c3e6efc84b47869b76e6924f90c3f30264
Prashant Paliwal <prashantpaliwal203@gmail.com>
2022-06-10 21:32:11 +0530

    Customised code of reseller and management portal

--- registration/email.py
+++ registration/email.py
@@ -3,10 +3,24 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
-def send_forgot_password_mail(email,otp):
+def send_forgot_password_mail(email,token):
     subject = 'Forgot password email'
-    message = f'Hi, The OTP to reset your password is {otp}'
+    message = f'Hi, The link to reset your password is http://127.0.0.1:8000/reset_pass_link/{token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list=[email]
     send_mail(subject, message, email_from, recipient_list)
     return True
+
+# def send_otp(mobile=None,otp=None):
+#     # account_sid='AC494b0549e21944244dc76e673cd5bdb1'
+#     # auth_token='74fcd6a8087731d5b94027d491899546'
+#     # client=Client(account_sid,auth_token)
+#     # message=client.messages.create(
+#     #                                 body=f'The otp is -{otp}',
+#     #                                 from_='+14405381381',
+#     #                                 to='+919759525301'
+#     #                             )
+#     print('Your otp is:', otp)
+#     print('Your mobile is:', mobile)
+
+#     return True
--- registration/urls.py
+++ registration/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('login/', CustomObtainAuthToken.as_view(), name='auth-token'),
     path('register_customer/', RegisterCustomer.as_view()),
     path('forgot_password/', ForgotPassword.as_view(), name='forgotpassword'),
-    path('reset_password/', ResetPassword.as_view(), name='resetpassword'),
+    path('reset_pass_link/<token>', ResetPasswordToken.as_view(), name='resetpasstoken'),
+    path('reset_pass_link/', ResetPasswordToken.as_view(), name='resetpass'),
+    path('verify_otp/', VerifyOtpCustomer.as_view(), name='resetpassword'),
+    path('change_password/', ChangePassword.as_view(), name='changepassword'),
 ]
