4ce4dd57f57a4f52150286713fb83c8de65cda02
Server <server@vinrate.com>
2022-06-11 19:08:23 +0000

    server changes

--- Nature360/settings.py
+++ Nature360/settings.py
@@ -130,10 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+#STATIC_URL = 'static/'
 STATIC_URL = 'static/'
+STATIC_ROOT = '/mnt/volume-nyc1-01/html/MLM_WebApi/images/'
 # Note: Replace 'supersecure.codes' with your domain
-STATIC_ROOT = "/mnt/volume-nyc1-01/html/static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATIC_ROOT = "/mnt/volume-nyc1-01/html/MLM_WebApi/static"
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
